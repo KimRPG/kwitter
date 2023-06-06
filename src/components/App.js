10e5a68959e9e9d392efae2c798424876ee5b2e2
Kim-Hyun-Seung <120882082+Kim-Hyun-Seung@users.noreply.github.com>
2023-06-06 18:00:48 +0900

    input 생성

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import AppRouter from "./Router.js";
+import authService from "../fbase.js"
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
-    <AppRouter></AppRouter>
+    <>
+    <header> hi </header>
+    <AppRouter isLoggedIn = {isLoggedIn}></AppRouter>
+    <footer>&copy; </footer>
+    </>
   );
 }
 
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,6 +1,70 @@
 import React from "react";
+import { useState } from "react";
+import { styled } from "styled-components";
+
+
+function EmailInput({ inputValue, onChange }) {
+  return (
+    <Styleinput
+      name="email"
+      type="email"
+      placeholder="Email"
+      required
+      value={inputValue}
+      onChange={onChange}
+    />
+  );
+}
+
+function PwdInput({ inputValue, onChange }) {
+  return (
+    <input
+      name="password"
+      type="password"
+      placeholder="Password"
+      required
+      value={inputValue}
+      onChange={onChange}
+    />
+  );
+}
+const Styleinput = styled.input`
+background-color: black;
+color: aqua;
+`
 
 const Auth = () => {
-  return <span>Auth</span>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (e) => {
+    const { target: { name, value } } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+      console.log(value);
+    }
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <EmailInput inputValue={email} onChange={onChange} />
+        <PwdInput inputValue={password} onChange={onChange} />
+        <input type="submit" value="Log In" />
+      </form>
+
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </>
+  );
 }
+
 export default Auth;
